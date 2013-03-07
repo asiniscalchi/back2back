6570c862d0f9c5304421be0da1574494426f76d0
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2013-03-07 18:19:00 +0100

    [difftext] make it multiplatform

--- b2btest/difftext.py
+++ b2btest/difftext.py
@@ -1,13 +1,18 @@
 #! /usr/bin/python
-import os
+import os, difflib
 from ansi_color import ansiColor
 
 def differences(expected, result, diffbase, extra_args) :
 	extension = os.path.splitext(result)[-1]
 	difftxt = diffbase+extension
-	are_equal = os.system("diff %s %s > %s" % (expected, result, difftxt) ) == 0
-	if are_equal: 
-		os.system("rm %s" % difftxt)
+
+	expectedLines = open(expected, 'U').readlines()
+	resultLines = open(result, 'U').readlines()
+	diff = list(difflib.unified_diff(expectedLines, resultLines, expected, result))
+	open(difftxt, "w").writelines(diff)
+
+	if not diff:
+		os.remove(difftxt)
 		return []
 	return [
 		"The result file %s"%(result) + " is different to the expected %s"%(expected),
